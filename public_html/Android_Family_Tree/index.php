704af812ac6bbfff52300a4d9c6ded0c89604ce7
yamato <yamayamato8@gmail.com>
2017-10-29 12:14:35 +0900

    リンク等の環境設定

--- public_html/Android_Family_Tree/index.php
+++ public_html/Android_Family_Tree/index.php
@@ -1,6 +1,9 @@
 <?php
-require_once 'Zend/Controller/Front.php';
-date_default_timezone_set('Asia/Tokyo');
+	$path = '.:/virtual/htdocs/Android_Family_Tree/';
+    set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
-Zend_Controller_Front::getInstance()->setParam('noViewRenderer', true);
-Zend_Controller_Front::run('../../Android_Family_Tree/application/controllers');
+    require_once 'Zend/Controller/Front.php';
+
+    date_default_timezone_set('Asia/Tokyo');    // 日本のタイムゾーンに設定
+    Zend_Controller_Front::getInstance()->setParam('noViewRenderer', true);
+    Zend_Controller_Front::run('../../Android_Family_Tree/application/controllers');
